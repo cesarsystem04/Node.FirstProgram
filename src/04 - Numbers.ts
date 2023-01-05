dcdedbfb54bf8735a00f9aa75e4d91fc6db4ccfa
Cesar Torres Garcia <c.torres@condumex.com.mx>
2023-01-05 11:57:04 -0600

    Se agrega el ejercicio 4 donde se manipulan variables del tipo Numbrer.

---
+++ src/04 - Numbers.ts
@@ -0,0 +1,18 @@
+(() =>
+{
+let productPrice = 1000;
+productPrice = 12;
+console.log('productPrice', productPrice);
+
+let customerAge: number = 28;
+customerAge =1;
+console.log('customerAge', customerAge);
+
+let producInStock: number = 10;
+
+console.log('producInStock', producInStock);
+
+let discount = parseInt('123');
+console.log('producInstock', discount);
+
+})();
